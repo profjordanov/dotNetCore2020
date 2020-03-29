64d8952333d36017ebc1c0c5eb6d32d0cc0094e3
Jordan Jordanov <d_yugioh@abv.bg>
2020-03-29 07:50:45 +0300

    C# Design Patterns: Prototype

---
+++ DesignPatterns/Prototype Design Pattern/Prototype Pattern/Prototype Pattern/FoodOrder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype_Pattern
+{
+    public abstract class Prototype
+    {
+        public abstract Prototype ShallowCopy();
+        public abstract Prototype DeepCopy();
+        public abstract void Debug();
+    }
+
+    public class OrderInfo
+    {
+        public int id;
+
+        public OrderInfo(int id)
+        {
+            this.id = id;
+        }
+    }
+
+    public class FoodOrder: Prototype
+    {
+        public string customerName;
+        public bool isDelivery;
+        public string[] orderContents;
+        public OrderInfo info;
+
+        public FoodOrder(string name, bool delivery, string[] contents, OrderInfo info)
+        {
+            this.customerName = name;
+            this.isDelivery = delivery;
+            this.orderContents = contents;
+            this.info = info;
+        }
+
+        public override Prototype ShallowCopy()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+
+        public override Prototype DeepCopy()
+        {
+            FoodOrder clonedOrder = (FoodOrder)this.MemberwiseClone();
+            clonedOrder.info = new OrderInfo(this.info.id);
+
+            return clonedOrder;
+        }
+
+        public override void Debug()
+        {
+            Console.WriteLine("------- Prototype Food Order -------");
+            Console.WriteLine("\nName: {0} \nDelivery: {1}", this.customerName, this.isDelivery);
+            Console.WriteLine("ID: {0}", this.info.id);
+            Console.WriteLine("Order Contents: " + string.Join(",", orderContents) + "\n");
+        }
+    }
+
+    public class PrototypeManager
+    {
+        private Dictionary<string, Prototype> _prototypeLibrary = new Dictionary<string, Prototype>();
+        
+        public Prototype this[string key]
+        {
+            get { return _prototypeLibrary[key]; }
+            set { _prototypeLibrary.Add(key, value); }
+        }
+    }
+}
---
+++ DesignPatterns/Prototype Design Pattern/Prototype Pattern/Prototype Pattern/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Prototype_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Original order:");
+            FoodOrder savedOrder = new FoodOrder("Harrison", 
+                                                    true, 
+                                                    new string[] { "Pizza", "Coke"},
+                                                    new OrderInfo(2345));
+            savedOrder.Debug();
+
+            Console.WriteLine("Cloned order:");
+            FoodOrder clonedOrder = (FoodOrder)savedOrder.DeepCopy();
+            clonedOrder.Debug();
+
+            Console.WriteLine("Order changes:");
+            savedOrder.customerName = "Jeff";
+            savedOrder.info.id = 5555;
+            savedOrder.Debug();
+            clonedOrder.Debug();*/
+
+            PrototypeManager manager = new PrototypeManager();
+            manager["2/3/2020"] = new FoodOrder("Steve",
+                                                    false,
+                                                    new string[] { "Chicken Parm", "Root Beer" },
+                                                    new OrderInfo(8912));
+
+            Console.WriteLine("Manager clone:");
+            FoodOrder managerOrder = (FoodOrder)manager["2/3/2020"].DeepCopy();
+            managerOrder.Debug();
+        }
+    }
+}
