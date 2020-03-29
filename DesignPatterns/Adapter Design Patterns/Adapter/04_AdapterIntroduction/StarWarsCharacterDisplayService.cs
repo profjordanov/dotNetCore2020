54365dbf5343d407faa92c4a6c27ed4985e49c44
Jordan Jordanov <d_yugioh@abv.bg>
2020-03-29 13:34:51 +0300

    C# Design Patterns: Adapter

---
+++ DesignPatterns/Adapter Design Patterns/Adapter/04_AdapterIntroduction/StarWarsCharacterDisplayService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Adapter.AdapterIntroduction
+{
+    public class StarWarsCharacterDisplayService
+    {
+        public async Task<string> ListCharacters(CharacterSource source)
+        {
+            ICharacterSourceAdapter characterSource;
+            if (source == CharacterSource.File)
+            {
+                string filePath = @"Adapter/People.json";
+                characterSource = new CharacterFileSourceAdapter(filePath);
+            } 
+            else if (source == CharacterSource.Api)
+            {
+                characterSource = new StarWarsApiCharacterSourceAdapter();
+            }
+            else
+            {
+                throw new Exception("Invalid character source");
+            }
+            
+            var people = await characterSource.GetCharacters();
+
+            var sb = new StringBuilder();
+            int nameWidth = 30;
+            sb.AppendLine($"{"NAME".PadRight(nameWidth)}   {"HAIR"}");
+            foreach (Person person in people)
+            {
+                sb.AppendLine($"{person.Name.PadRight(nameWidth)}   {person.HairColor}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
